90109517b9c1b9e6db5e8d9f8743dee20d54aed4
Mariyam-bs23 <mariyam.begom@bs-23.com>
2024-03-03 19:46:44 +0600

    taco design implementation

---
+++ src/main/java/com/example/tacocloud/Ingredients.java
@@ -0,0 +1,13 @@
+package com.example.tacocloud;
+
+import lombok.Data;
+@Data
+public class Ingredients {
+    private final String id;
+    private final String name;
+    private final Type type;
+
+    public enum Type{
+        WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
+    }
+}
